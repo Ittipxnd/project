b756f5cd1a9cddd82daf7718b456b9d3a2f18eeb
Chanisatabarry <156068781+Chanisatabarry@users.noreply.github.com>
2024-07-24 18:19:07 +0700

    ลบ

--- mark1/android/app/src/main/kotlin/com/example/mark1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mark1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- mark2/android/app/src/main/kotlin/com/example/mark2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mark2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
