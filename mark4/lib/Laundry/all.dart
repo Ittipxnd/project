5ebf4b2b65ab253f8ee2642342591515f976148b
Chanisatabarry <156068781+Chanisatabarry@users.noreply.github.com>
2024-08-04 19:38:02 +0700

    แก้หน้าhomeเสร็จแล้ว

---
+++ mark4/lib/Laundry/all.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageLaundry extends StatelessWidget {
+  const PageLaundry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- mark4/lib/one/home.dart
+++ mark4/lib/one/home.dart
@@ -41,76 +41,121 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 16),
-                const Text(
-                  'หมวดหมู่ผลิตภัณฑ์',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Category',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 16),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 1',
-                      ),
-                      const SizedBox(width: 8),
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 2',
-                      ),
-                      const SizedBox(width: 8),
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 3',
-                      ),
-                    ],
+                _buildProductRow([
+                  ProductData(
+                    image: const AssetImage('assets/image/laundry.jpg'),
+                    productName: 'Laundry',
+                  ),
+                  ProductData(
+                    image: const AssetImage('assets/image/hair.jpg'),
+                    productName: 'Hair Shampoo',
+                  ),
+                  ProductData(
+                    image: const AssetImage('assets/image/snack.jpg'),
+                    productName: 'Snack',
+                  ),
+                  ProductData(
+                    image: const AssetImage('assets/image/mama.jpg'),
+                    productName: 'Instant Noodle',
+                  ),
+                  ProductData(
+                    image: const AssetImage('assets/image/milk-1.jpg'),
+                    productName: 'Milk',
+                  ),
+                ]),
+                const SizedBox(height: 16),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Featured Products',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 16),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 4',
-                      ),
-                      const SizedBox(width: 8),
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 5',
-                      ),
-                      const SizedBox(width: 8),
-                      ProductCard(
-                        imageUrl: 'https://via.placeholder.com/153',
-                        productName: 'Product 6',
-                      ),
-                    ],
+                _buildProductRow([
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/84/48/4893049120084/4893049120084_1-20240625120130-.jpg',
+                    ),
+                    productName:
+                        'ริทซ์ แครกเกอร์ สอดไส้ครีมรสชีส 27 ก. แพ็ค 12',
                   ),
-                ),
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/25/88/8850987143625/8850987143625_1-20240305181413-.jpg',
+                    ),
+                    productName:
+                        'มาม่า บะหมี่กึ่งสำเร็จรูป รสหมูสับต้มยำน้ำข้น 60 ก. แพ็ค 10',
+                  ),
+                ]),
                 const SizedBox(height: 16),
-                Column(
-                  children: [
-                    ProductCard(
-                      imageUrl: 'https://via.placeholder.com/153',
-                      productName: 'Product 4',
+                _buildProductRow([
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/09/76/7622201450809/7622201450809_1-20240620174106-.jpg',
                     ),
-                    const SizedBox(height: 8),
-                    ProductCard(
-                      imageUrl: 'https://via.placeholder.com/153',
-                      productName: 'Product 5',
+                    productName: 'แคดเบอรีแดรีมิลค์วิทโอรีโอ15ก.แพ็ค10ซอง',
+                  ),
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/76/49/4987176209276/4987176209276-20240515162357-.jpg',
                     ),
-                    const SizedBox(height: 8),
-                    ProductCard(
-                      imageUrl: 'https://via.placeholder.com/153',
-                      productName: 'Product 6',
+                    productName:
+                        'ดาวน์นี่น้ำยาซักผ้าซันไรท์เฟรชคลีน460มล.แพ็ค2',
+                  ),
+                ]),
+                const SizedBox(height: 16),
+                _buildProductRow([
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/29/88/8851932431729/8851932431729_1-20240424160111-.jpg',
                     ),
-                  ],
-                ),
+                    productName:
+                        'ริทซ์ แครกเกอร์ สอดไส้ครีมรสชีส 27 ก. แพ็ค 12',
+                  ),
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/25/88/8850987143625/8850987143625_1-20240305181413-.jpg',
+                    ),
+                    productName:
+                        'มาม่า บะหมี่กึ่งสำเร็จรูป รสหมูสับต้มยำน้ำข้น 60 ก. แพ็ค 10',
+                  ),
+                ]),
+                const SizedBox(height: 16),
+                _buildProductRow([
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/84/48/4893049120084/4893049120084_1-20240625120130-.jpg',
+                    ),
+                    productName:
+                        'ริทซ์ แครกเกอร์ สอดไส้ครีมรสชีส 27 ก. แพ็ค 12',
+                  ),
+                  ProductData(
+                    image: NetworkImage(
+                      'https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/25/88/8850987143625/8850987143625_1-20240305181413-.jpg',
+                    ),
+                    productName:
+                        'มาม่า บะหมี่กึ่งสำเร็จรูป รสหมูสับต้มยำน้ำข้น 60 ก. แพ็ค 10',
+                  ),
+                ]),
               ],
             ),
           ),
@@ -118,37 +163,77 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  // Helper method to build a row of products
+  Widget _buildProductRow(List<ProductData> products) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: products
+            .map((product) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: ProductCard(
+                    image: product.image,
+                    productName: product.productName,
+                    width: 90,
+                    height: 90,
+                    onPressed: () {},
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class ProductData {
+  final ImageProvider image;
+  final String productName;
+
+  ProductData({required this.image, required this.productName});
 }
 
 class ProductCard extends StatelessWidget {
-  final String imageUrl;
+  final ImageProvider image;
   final String productName;
+  final VoidCallback onPressed;
+  final double width;
+  final double height;
 
   const ProductCard({
     Key? key,
-    required this.imageUrl,
+    required this.image,
     required this.productName,
+    required this.onPressed,
+    this.width = 50.0, // Default width
+    this.height = 50.0, // Default height
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.network(
-          imageUrl,
-          width: 153,
-          height: 153,
-          fit: BoxFit.cover,
-        ),
-        const SizedBox(height: 8),
-        Text(
-          productName,
-          style: const TextStyle(
-            fontSize: 12,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10), // Rounded corners
+            child: Image(
+              image: image,
+              width: width,
+              height: height,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 8),
+          Text(
+            productName,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
